c37b3c5bff7aeb164884bfe0eaa53eaf69e31a27
ThaomaisomAum <107332300+ThaomaisomAum@users.noreply.github.com>
2022-06-26 09:58:23 +0700

    update day3

--- modulpackge/my_math_use.py
+++ modulpackge/my_math_use.py
@@ -5,7 +5,7 @@ print(f'aqart of 5 = {sqrt(5)}')
 print(f'circle_area = {circle_area(2):,.2f}')
 
 #วิธีที่2
-import my_match
+import my_match as my
 print("****วิธีที่2****")
-print(f'aqart of 5 = {sqrt(5)}')
-print(f'circle_area = {circle_area(2):,.2f}')
+print(f'aqart of 5 = {my.sqrt(5)}')
+print(f'circle_area = {my.circle_area(2):,.2f}')
