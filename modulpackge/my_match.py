ed5dabc0ae00628db9bdc284eb6521295065d917
ThaomaisomAum <107332300+ThaomaisomAum@users.noreply.github.com>
2022-06-26 09:53:06 +0700

    updatd day3

---
+++ modulpackge/my_match.py
@@ -0,0 +1,7 @@
+PI = 3.1416
+
+def sqrt(number):
+    return number ** 2
+
+def circle_area(radius):
+    return PI *(radius ** 2)
---
+++ modulpackge/my_math_use.py
@@ -0,0 +1,11 @@
+#วิธีที่1
+from my_match import sqrt,circle_area
+
+print(f'aqart of 5 = {sqrt(5)}')
+print(f'circle_area = {circle_area(2):,.2f}')
+
+#วิธีที่2
+import my_match
+print("****วิธีที่2****")
+print(f'aqart of 5 = {sqrt(5)}')
+print(f'circle_area = {circle_area(2):,.2f}')
