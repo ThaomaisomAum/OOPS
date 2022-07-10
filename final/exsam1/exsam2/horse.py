c7c6ef2ee831662652a3fc6bcbf7b3e6cc82d24e
ThaomaisomAum <107332300+ThaomaisomAum@users.noreply.github.com>
2022-07-10 11:24:57 +0700

    exam1

---
+++ final/exsam1/exsam2/horse.py
@@ -0,0 +1,23 @@
+class Horse:
+    def __init__(self, max_height:float, name:str, color:str) -> None:
+        super().__init__(max_height, name, color)
+        self.max_height = "200"
+        self.name = "Khan Khan"
+        self.color = "Grey"
+
+
+    def run(self):
+        self.run = "Khan Khan is running"
+        return(f'run: {self.run}')
+
+    def show_name(self):
+        return f'Name: {self.name}'
+
+    def show_info(self):
+        print(f'Name: {self.name}')
+        print(f'Color: {self.color}')
+        print(f'Max Height: {self.max_height}')
+        
+
+
+    
